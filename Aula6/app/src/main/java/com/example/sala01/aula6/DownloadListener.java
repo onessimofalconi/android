2136ae35bf1fb93f5f74dd6f4227db68848034e9
sala01 <sala01@targettrust.local>
2017-05-15 12:37:16 -0300

    aula06

---
+++ Aula6/app/src/main/java/com/example/sala01/aula6/DownloadListener.java
@@ -0,0 +1,8 @@
+package com.example.sala01.aula6;
+
+import android.graphics.Bitmap;
+
+public interface DownloadListener {
+
+    void getImg(Bitmap bitmap);
+}
---
+++ Aula6/app/src/main/java/com/example/sala01/aula6/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.sala01.aula6;
+
+import android.graphics.Bitmap;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageView = (ImageView) findViewById(R.id.imageView);
+        ProgressBar progressBar = (ProgressBar) findViewById(R.id.progressBar);
+
+        DownloadImage downloadImage = new DownloadImage(this, progressBar,
+                new DownloadListener() {
+            @Override
+            public void getImg(Bitmap bitmap) {
+                imageView.setImageBitmap(bitmap);
+            }
+        });
+
+        downloadImage.execute();
+
+    }
+}
